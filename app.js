a034d3c87ccd1ef61a79987d9ac47d609ac46f03
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-04-12 06:01:23 +0530

    task 2

--- app.js
+++ app.js
@@ -1,52 +1,65 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
 
-   if(url==='/'){
-     //form
-     res.setHeader('Content-Type', 'text/html');
+  if (url === '/' && method === 'GET') {
+    
+    fs.readFile('data.txt', (err, data) => {
+      res.setHeader('Content-Type', 'text/html');
+      const messages = err ? '' : data.toString().replace(/\n/g, '<br>');
 
-     res.end(`<form action="/message" method="POST">
-      <label>Enter Text: </label>
-      <input type="text" name="username" />
-      <button type="submit">Add</button>
-      </form>`)
+      res.end(`
+        <html>
+          <body>
+            <h2>Messages:</h2>
+            <div>${messages}</div>
+            <hr>
+            <form action="/" method="POST">
+              <label>Enter Text: </label>
+              <input type="text" name="username" />
+              <button type="submit">Add</button>
+            </form>
+          </body>
+        </html>
+      `);
+    });
+  }
 
+  else if (url === '/' && method === 'POST') {
+    let body = [];
 
-   }
-   else {
-    if(url==='/message'){
-      
-      res.setHeader("Content-Type", "text/html");
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
 
-      let body = [];
-      req.on("data", (chunks)=>{
-        body.push(chunks);
-      })
+    req.on("end", () => {
+      const buffer = Buffer.concat(body);
+      const formData = buffer.toString();
+      const formValue = decodeURIComponent(formData.split('=')[1].replace(/\+/g, ' '));
 
-      req.on("end", ()=>{
-        let buffer = Buffer.concat(body);
-        let formData = buffer.toString();
-        let formValue = formData.split('=')[1];
+      // Read existing messages, prepend new message
+      fs.readFile('data.txt', (err, data) => {
+        const previousData = err ? '' : data.toString();
+        const newData = formValue + '\n' + previousData;
 
-        fs.writeFile("data.txt", formValue, (err)=>{
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        res.end();
-      })
-      } )
+        fs.writeFile('data.txt', newData, (err) => {
+          res.statusCode = 302;
+          res.setHeader('Location', '/');
+          res.end();
+        });
+      });
+    });
+  }
 
-     
+  else {
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>404 - Page Not Found</h1>');
+  }
+});
 
-    }
-   }
-
-
-})
-
-server.listen(3000, ()=>{
-  console.log("server is running.")
-})
+server.listen(3000, () => {
+  console.log("Server is running.");
+});
